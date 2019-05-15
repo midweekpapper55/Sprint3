11262810d92c3a3f075e62f8885622722b672fe5
Admin <Admin@DESKTOP-VDOLDMS>
2019-05-15 10:51:38 -0500

    Se desarrolló la automatización de la Hisroria de usuario Uno.

---
+++ Test Suites/Green Mobile.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Green Mobile</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>d4539c3b-064c-4b0d-8aa8-f48104af0c8e</testSuiteGuid>
+   <testCaseLink>
+      <guid>46e78ec4-ae20-4540-9bc4-788d40af41bf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1. Ingreso a la Aplicacion</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
